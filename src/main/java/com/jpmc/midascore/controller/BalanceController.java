8d322d8deed34083093fbf220866428f90cc6dc9
Dharshini-457 <dharshinir619@gmail.com>
2026-01-11 23:31:01 +0530

    task 5 commit

---
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -0,0 +1,29 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+
+    private final UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public Balance getBalance(@RequestParam("userId") long userId) {
+        UserRecord user = userRepository.findById(userId);
+        if (user == null) {
+            return new Balance(0);
+        }
+        return new Balance(user.getBalance());
+    }
+}
