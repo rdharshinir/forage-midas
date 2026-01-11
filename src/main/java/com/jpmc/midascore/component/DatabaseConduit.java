6cdc8df379fb14c9d8d23d99d995b82b9acfc1d3
Dharshini-457 <dharshinir619@gmail.com>
2026-01-11 23:30:32 +0530

    task3 commit

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,90 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
 
-    public DatabaseConduit(UserRepository userRepository) {
+    private static final Logger LOG = LoggerFactory.getLogger(DatabaseConduit.class);
+
+    public DatabaseConduit(UserRepository userRepository, TransactionRepository transactionRepository, RestTemplate restTemplate) {
         this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplate;
+    }
+
+    @KafkaListener(topics = "transactions", groupId = "midas-core")
+    @Transactional
+    public void processTransaction(Transaction transaction) {
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        if (sender == null) {
+            LOG.error("Sender with id {} not found", transaction.getSenderId());
+            return;
+        }
+
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+        if (recipient == null) {
+            LOG.error("Recipient with id {} not found", transaction.getRecipientId());
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            LOG.error("Sender with id {} has insufficient funds", transaction.getSenderId());
+            return;
+        }
+
+        float incentiveAmount = 0.0f;
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<Transaction> requestEntity = new HttpEntity<>(transaction, headers);
+
+            ResponseEntity<Incentive> response = restTemplate.exchange(
+                    "http://localhost:8080/incentive",
+                    HttpMethod.POST,
+                    requestEntity,
+                    Incentive.class
+            );
+
+            if (response.getBody() != null && response.getBody().getAmount() >= 0) {
+                incentiveAmount = response.getBody().getAmount();
+            }
+        } catch (RestClientException e) {
+            LOG.error("Error fetching incentive for transaction {}: {}", transaction, e.getMessage());
+            // Proceed without incentive if API call fails
+        }
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
+
+        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount));
+        userRepository.save(sender);
+        userRepository.save(recipient);
+
+        LOG.info("Transaction processed: {}", transaction);
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
     }
-
 }
--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -1,5 +1,6 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@EmbeddedKafka(partitions = 1)
 public class TaskThreeTests {
     static final Logger logger = LoggerFactory.getLogger(TaskThreeTests.class);
 
@@ -23,6 +24,9 @@ public class TaskThreeTests {
     @Autowired
     private FileLoader fileLoader;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void task_three_verifier() throws InterruptedException {
         userPopulator.populate();
@@ -30,17 +34,12 @@ public class TaskThreeTests {
         for (String transactionLine : transactionLines) {
             kafkaProducer.send(transactionLine);
         }
-        Thread.sleep(2000);
+        Thread.sleep(5000);
 
 
         logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
-        logger.info("use your debugger to find out what waldorf's balance is after all transactions are processed");
-        logger.info("kill this test once you find the answer");
-        while (true) {
-            Thread.sleep(20000);
-            logger.info("...");
-        }
+        logger.info("The balance for waldorf is: {}", userRepository.findByName("waldorf").getBalance());
     }
 }
